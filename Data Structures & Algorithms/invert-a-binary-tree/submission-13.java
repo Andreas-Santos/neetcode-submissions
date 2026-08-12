39fa93f2d6804ff2fb957dca57b08020767eb4ba
Andreas Santos <119018805+Andreas-Santos@users.noreply.github.com>
2026-08-11 21:05:09 -0300

    Add: invert-a-binary-tree - submission-13

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-13.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+        // BFS implementation
+
+        if(root == null) return null;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+
+            TreeNode current = queue.poll();
+
+            if(current.right != null) queue.offer(current.right);
+            
+            if(current.left != null) queue.offer(current.left);
+
+            TreeNode temp = current.right;
+            current.right = current.left;
+            current.left = temp;
+        }
+
+        return root;
+    }
+}
