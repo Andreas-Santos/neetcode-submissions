5c2f0c48232c118958cdfef2b6f5b9ecdbd3d771
Andreas Santos <119018805+Andreas-Santos@users.noreply.github.com>
2026-08-20 18:04:27 -0300

    Add: three-integer-sum - submission-25

---
+++ Data Structures & Algorithms/three-integer-sum/submission-25.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        Arrays.sort(nums);
+        
+        List<List<Integer>> triplets = new ArrayList<>();
+        
+        for(int i = 0; i < nums.length; i++) {
+
+            if(nums[i] > 0)
+                break;
+            
+            if(i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int l = i + 1;
+            int r = nums.length - 1;
+            while(l < r) {
+                
+                // if(nums[r] < 0)
+                //     break;
+
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if(sum > 0) {
+                    r--;
+                }
+                else if(sum < 0) {
+                    l++;
+                }
+                else {
+                    triplets.add(new ArrayList<>(List.of(nums[i], nums[l], nums[r])));
+                    l++;
+                    r--;
+                    while(l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+                }
+            }
+        }
+
+        return triplets;
+    }
+}
