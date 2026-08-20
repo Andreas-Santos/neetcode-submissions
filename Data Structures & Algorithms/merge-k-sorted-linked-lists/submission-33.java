252b1e7366765bd142723fc91f7accb7bd08bcc5
Andreas Santos <119018805+Andreas-Santos@users.noreply.github.com>
2026-08-20 20:19:05 -0300

    Add: merge-k-sorted-linked-lists - submission-33

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-33.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0)
+            return null;
+
+        if(lists.length == 1)
+            return lists[0];
+
+        while(lists.length > 1) {
+
+            List<ListNode> mergedLists = new ArrayList<>();
+            for(int i = 0; i < lists.length; i+=2) {
+                ListNode l1 = lists[i];
+                ListNode l2 = i + 1 < lists.length ? lists[i + 1] : null;
+                ListNode mergedList = mergeLists(l1, l2);
+                mergedLists.add(mergedList);
+            }
+
+            ListNode[] newList = new ListNode[mergedLists.size()];
+            for(int i = 0; i < mergedLists.size(); i++) {
+                newList[i] = mergedLists.get(i);
+            }
+
+            lists = newList;
+        }
+
+        return lists[0];
+    }
+
+    public ListNode mergeLists(ListNode l1, ListNode l2) {
+
+        if(l1 == null)
+            return l2;
+
+        if(l2 == null)
+            return l1;
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        while(l1 != null && l2 != null) {
+            if(l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+
+            current = current.next;
+        }
+
+        if(l1 != null) {
+            current.next = l1;
+        }
+        else {
+            current.next = l2;
+        }
+
+        return dummy.next;
+    } 
+}
