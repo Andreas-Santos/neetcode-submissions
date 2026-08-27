e751d2dea8934381bea656bbec51e3c5b7525f95
Andreas Santos <119018805+Andreas-Santos@users.noreply.github.com>
2026-08-26 23:13:30 -0300

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        return dfs(nums, 0);
+    }
+
+    public List<List<Integer>> dfs(int[] nums, int index) {
+        if(index == nums.length) {
+            List<List<Integer>> perms = new ArrayList<>();
+            perms.add(new ArrayList<>());
+            return perms;
+        }
+
+        List<List<Integer>> permutationsList = new ArrayList<>();
+        List<List<Integer>> perms = dfs(nums, index + 1);
+        for(List<Integer> perm : perms) {
+            for(int i = 0; i < perm.size() + 1; i++) {
+                List<Integer> permCopy = new ArrayList<>(perm);
+                permCopy.add(i, nums[index]);
+                permutationsList.add(permCopy);
+            }
+        }
+        
+        return permutationsList;
+    }
+}
