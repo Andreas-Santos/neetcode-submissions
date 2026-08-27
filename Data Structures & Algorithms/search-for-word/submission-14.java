d26c1af58d5362a04661b5535c6ec60ca97fa09c
Andreas Santos <119018805+Andreas-Santos@users.noreply.github.com>
2026-08-27 15:05:53 -0300

    Add: search-for-word - submission-14

---
+++ Data Structures & Algorithms/search-for-word/submission-14.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    private int ROWS;
+    private int COLS;
+    boolean[][] visited;
+
+    public boolean exist(char[][] board, String word) {
+
+        ROWS = board.length;
+        COLS = board[0].length;
+        visited = new boolean[ROWS][COLS];
+
+        for(int r = 0; r < ROWS; r++) {
+            for(int c = 0; c < COLS; c++) {
+                if(board[r][c] == word.charAt(0) 
+                    && dfs(board, r, c, 0, word)
+                ) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean dfs(char[][] board, int r, int c, int i, String word) {
+
+        if(i == word.length()) {
+            return true;
+        }
+
+        if(r < 0 || r >= ROWS || c < 0 || c >= COLS || visited[r][c] || board[r][c] != word.charAt(i)) {
+            return false;
+        }
+
+        visited[r][c] = true;
+
+        boolean existsWord = 
+                    dfs(board, r + 1, c, i + 1, word) ||
+                    dfs(board, r - 1, c, i + 1, word) ||
+                    dfs(board, r, c + 1, i + 1, word) ||
+                    dfs(board, r, c - 1, i + 1, word);
+
+        visited[r][c] = false;
+
+        return existsWord;
+    }
+}
