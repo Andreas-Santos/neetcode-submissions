9636a5909f297658c05ac16dce3e71c68cbdb4a1
Andreas Santos <119018805+Andreas-Santos@users.noreply.github.com>
2026-08-29 20:02:45 -0300

    Add: longest-palindromic-substring - submission-8

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-8.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        
+        int longestSize = 0;
+        int longestStart = -1;
+
+        for(int i = 0; i < s.length(); i++) {
+            int l = i;
+            int r = i;
+
+            while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                if(longestSize < r - l + 1) {
+                    longestSize = r - l + 1;
+                    longestStart = l;
+                }
+
+                l--;
+                r++;
+            }
+
+            l = i;
+            r = i+1;
+            while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                if(longestSize < r - l + 1) {
+                    longestSize = r - l + 1;
+                    longestStart = l;
+                }
+
+                l--;
+                r++;
+            }
+        }
+
+        if(longestStart == -1) {
+            return "";
+        }
+
+        return s.substring(longestStart, longestStart + longestSize);
+    }
+}
