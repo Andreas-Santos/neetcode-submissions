9966e24bd3ba5c69c54d921612a93bdad3555892
Andreas Santos <119018805+Andreas-Santos@users.noreply.github.com>
2026-09-01 23:57:02 -0300

    Add: palindrome-partitioning - submission-5

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-5.java
@@ -0,0 +1,42 @@
+class Solution {
+    private List<List<String>> result;
+    private List<String> current;
+
+    public List<List<String>> partition(String s) {
+        result = new ArrayList<>();
+        current = new ArrayList<>();
+
+        dfs(s, 0);
+
+        return result;
+    }
+
+    public void dfs(String s, int i) {
+        if(i == s.length()) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for(int j = i; j < s.length(); j++) {
+            if(isPalindrome(s, i, j)) {
+                current.add(s.substring(i, j + 1));
+                dfs(s, j + 1);
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+
+    public boolean isPalindrome(String s, int l, int r) {
+        while(l <= r) {
+
+            if(s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
