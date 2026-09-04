70b699c7fabf4887627b4291eeee94b13788749c
Andreas Santos <119018805+Andreas-Santos@users.noreply.github.com>
2026-09-03 22:46:19 -0300

    Add: reverse-string - submission-5

---
+++ Data Structures & Algorithms/reverse-string/submission-5.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void reverseString(char[] s) {
+        
+        if(s.length == 0 || s.length == 1) {
+            return;
+        }
+
+        int l = 0;
+        int r = s.length - 1;
+        while(l < r) {
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+
+            l++;
+            r--;
+        }
+    }
+}
